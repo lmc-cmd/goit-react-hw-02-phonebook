6a6b3afb631657e7aec06f237d1153ec48f4e2c4
lmc-cmd <63010584+lmc-cmd@users.noreply.github.com>
2022-08-01 10:26:52 +0300

    a

--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -1,15 +1,12 @@
-const Contacts = ({ state }) => {
-  state.map(el => {
-    return (
-      <>
-        <title>Contacts</title>
-        <ul>
-          <li>
-            1<span>{el.name}</span>
-          </li>
-        </ul>
-      </>
-    );
-  });
-};
-export default Contacts;
+// const Contacts = ({ state }) => {
+//   state.map(el => {
+//     let some = <p>{el.name}</p>;
+
+//     return (
+//       <>
+//         <p>{some}</p>
+//       </>
+//     );
+//   });
+// };
+// export default Contacts;
